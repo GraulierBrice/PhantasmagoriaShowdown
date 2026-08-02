2f5b2b0a29666eef78e7421db7fbd48225b87576
GraulierBrice <brice.graulier@gmail.com>
2026-08-02 21:48:47 +0200

    Swing back and forth fixes

--- Scenes/Patterns/pattern_swing.gd
+++ Scenes/Patterns/pattern_swing.gd
@@ -2,9 +2,24 @@
 extends PatternArc
 
 @export_range(1,10,1) var swings: int = 1
+var swings_counter: int = 0
 
-func preview():
+func execute(pattern_owner):
+	swings_counter = swings
 	for i in range(swings):
 		turn *= (-1 as Globals.ETurn)
 		angle_offset = fmod(angle_offset+PI,TAU)
-		super.preview()
+		await super.execute(pattern_owner)
+
+func preview():
+	swings_counter = swings
+	for i in range(swings):
+		turn *= (-1 as Globals.ETurn)
+		angle_offset = fmod(angle_offset+PI,TAU)
+		await super.preview()
+
+func _on_preview_done():
+	swings_counter -= 1
+	if swings_counter <= 0:
+		super._on_preview_done()
+	
